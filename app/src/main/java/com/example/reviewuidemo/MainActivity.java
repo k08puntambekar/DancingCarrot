ddf61de89b9c0f6f4363f0fac3da0a15742597bf
Kaushik <k08puntambekar@gmail.com>
2021-05-29 17:57:52 +0530

    ReviewUi-kaushik

---
+++ app/src/main/java/com/example/reviewuidemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.reviewuidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void reviewUI(View view) {
+        Intent reviewUI = new Intent(this, ReviewUI.class);
+        startActivity(reviewUI);
+    }
+
+}
